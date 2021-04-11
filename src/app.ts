6696f771ccfed53da46c304d88cf227f8921549a
Ivan Clavijo <ivanclabe@gmail.com>
2021-04-11 16:25:59 -0500

    domain/item

--- src/app.ts
+++ src/app.ts
@@ -35,10 +35,16 @@ class HttpException extends Error {
 
 // error handler
 app.use(
-  (err: HttpException, req: Request, res: Response, next: NextFunction) => {
+  (
+    err: HttpException,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
     // set locals, only providing error in development
     res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.locals.error =
+      req.app.get('env') === 'development' ? err : {};
 
     // render the error page
     res.status(err.status || 500);
